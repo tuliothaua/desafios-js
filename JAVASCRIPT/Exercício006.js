d2b622ca877159ec3dcf254569571fe6bf9a0f92
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-08-27 14:06:49 -0300

    Create Exercício006.js

---
+++ JAVASCRIPT/Exercício006.js
@@ -0,0 +1,23 @@
+// Túlio Thauã Dutra >>
+/* Contagem Regressiva! */
+
+for (i = 10; i >= 0; i--) {
+     console.log(i)
+      if(i === 0){
+         console.log(`BOOM!`)
+       }
+}
+// Saída Esperada: 
+/*
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+BOOM! */
