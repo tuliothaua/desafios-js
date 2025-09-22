8cb40ea4e378cf9216730b088bd2efc81f3a8332
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-09-22 16:05:16 -0300

    Create decades.js

---
+++ JAVASCRIPT/decades.js
@@ -0,0 +1,24 @@
+// ╔════════════════════════════════════════════════════╗
+// ║  AUTHOR : Túlio Thauã Dutra                        ║
+// ║  HANDLE : root@thaua                               ║
+// ║  STATUS : javascript Learning Mode >> ACTIVE       ║
+// ║  FILE   : decades.js                               ║
+// ╚════════════════════════════════════════════════════╝
+
+// Codédex - Decades
+
+let year = 1947
+
+if (year >= 1920 && year <= 1929){
+  console.log("Roaring Twenties")
+} else if (year >= 1930 && year <= 1939){
+  console.log("Dirty Thirties")
+} else if (year >= 1940 && year <= 1949){
+  console.log("Fighting Forties")
+} else if (year >= 1950 && year <= 1959){
+  console.log("Fabulous Fifties")
+} else if (year >= 1960 && year <= 1969){
+  console.log("Swinging Sixties")
+} else {
+  console.log("Year out of range")
+}
