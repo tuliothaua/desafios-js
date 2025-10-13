8d01b0e024cf48516f034b9f7a16e2639c2d2641
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-10-13 14:52:56 -0300

    refactor: move file to the 'labdex/' folder
    It move to the 'Códedex/labdex/' folder and add comments

---
+++ JAVASCRIPT/Códedex/Labdex/secretrecipe.js
@@ -0,0 +1,29 @@
+// ╔════════════════════════════════════════════════════╗
+// ║  AUTHOR : Túlio Thauã Dutra                        ║
+// ║  HANDLE : root@thaua                               ║
+// ║  STATUS : javascript Learning Mode >> ACTIVE       ║
+// ║  FILE   : secretrecipe.js                          ║
+// ╚════════════════════════════════════════════════════╝
+
+//Codedex - Secret Recipe
+
+//[INFO] :: Ingredients!
+console.log("The Egg Bread Recipe")
+console.log("Ingredients: ")
+console.log("1 Egg")
+console.log("1 Ketchup")
+console.log("1 Mayonnaise")
+console.log("1 Juice of your choice!")
+
+//[INFO] :: How to Prepare!
+console.log("How to Prepare:")
+console.log("1. Fry the egg")
+console.log("2. Open the bread")
+console.log("3. Put Ketchup inside")
+console.log("4. Put Mayonnaise inside")
+console.log("5. Put egg inside")
+console.log("6. Enjoy your meal with a juice!")
+
+//[INFO] :: Dont divulge!
+console.log("And dont forget to not divulge")
+console.log("ITS A SECRET RECIPE 🤫")
