f656da6f08850db8cd3f4ad13bf25746bd33faa0
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-10-13 14:25:04 -0300

    refactor: use let & const and rename the file
    This refactor implements let and const for learning how to use
    and rename the main file

--- JAVASCRIPT/Práticas Laboratório/arquivo.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello World")
-
-const nome = "Tulio"
-
-console.log("Hello my name is" + nome)
-
---
+++ JAVASCRIPT/Práticas Laboratório/let&const.js
@@ -0,0 +1,28 @@
+// ╔════════════════════════════════════════════════════╗
+// ║  AUTHOR : Túlio Thauã Dutra                        ║
+// ║  HANDLE : root@thaua                               ║
+// ║  STATUS : javascript Learning Mode >> ACTIVE       ║
+// ║  FILE   : let&const.js                             ║
+// ╚════════════════════════════════════════════════════╝
+
+// Codédex - Let & Const
+
+//[VAR] :: Creating the Variables!
+const firstName = "Tulio"
+const favoriteColor = "Green"
+let location = "Lages"
+let mood = "Happy"
+
+//[EXEC] :: Spittin on the screen!
+console.log(firstName)
+console.log(favoriteColor)
+console.log(location)
+console.log(mood)
+
+//[VAR] :: Changing the new Variables!
+location = "Anita"
+mood = "Sad"
+
+//[EXEC] :: Spittin on the screen!
+console.log(location)
+console.log(mood)
