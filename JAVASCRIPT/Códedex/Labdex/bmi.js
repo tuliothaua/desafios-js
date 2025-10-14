25f61fd4de3f5c35a8474a21c6618dcc316e5b3a
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-10-14 15:20:00 -0300

    fix(comment): corrected function description to BMI calculation
    Corrected the function comment which previously stated it was converting Fahrenheit to Celsius. The actual functionality is BMI calculation.

--- JAVASCRIPT/Códedex/Labdex/bmi.js
+++ JAVASCRIPT/Códedex/Labdex/bmi.js
@@ -12,7 +12,7 @@ let height = 1.70 ** 2
 let mass = 68
 let bmi;
 
-//[FUNC] :: Converting a number from fahrenheit to Celsius!
+//[FUNC] :: Calculating the BMI!
 bmi = mass / height 
 
 //[EXEC] :: Spittin on the screen!
