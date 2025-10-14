69022a6d7fc7b64522ce4e66d612adb23c90d43b
Túlio Thauã Dutra <tuliothauandutra@gmail.com>
2025-10-14 15:05:10 -0300

    feat: add a func to calculate bmi
    Add a function to calculate your BMI

---
+++ JAVASCRIPT/Códedex/Labdex/bmi.js
@@ -0,0 +1,19 @@
+// ╔════════════════════════════════════════════════════╗
+// ║  AUTHOR : Túlio Thauã Dutra                        ║
+// ║  HANDLE : root@thaua                               ║
+// ║  STATUS : javascript Learning Mode >> ACTIVE       ║
+// ║  FILE   : bmi.js                                   ║
+// ╚════════════════════════════════════════════════════╝
+
+// Codédex - BMI
+
+//[VAR] :: Creating the Variables!
+let height = 1.70 ** 2
+let mass = 68
+let bmi;
+
+//[FUNC] :: Converting a number from fahrenheit to Celsius!
+bmi = mass / height 
+
+//[EXEC] :: Spittin on the screen!
+console.log(bmi)
